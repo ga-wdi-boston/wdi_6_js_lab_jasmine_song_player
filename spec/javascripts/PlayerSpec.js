9b3b27eaf6aec76282574c36c3ad9e9db619151b
Alex Grant <grantovich@users.noreply.github.com>
2014-05-28 12:47:35 -0400

    Separate unit specs and use better beforeEach

--- spec/javascripts/PlayerSpec.js
+++ spec/javascripts/PlayerSpec.js
@@ -1,62 +1,48 @@
-describe("Song", function(){
-  describe("initializer", function(){
-    it("accepts a song title", function(){
-      var song = new Song("New Shoes");
-
-      expect(song.title).toEqual("New Shoes");
-    });
-  });
-});
-
-describe("Player", function() {
-  // Since we intend to assign `player` and `song` in a beforeEach, we must
-  // declare them up here -- if we declared them in the beforeEach, they
-  // would go out of scope immediately. Note this means we are responsible
-  // for cleanup of these variables between tests! (they're basically globals)
-  var player;
-  var song;
-
+// Remove the 'x' from `xdescribe` to activate this suite
+xdescribe("Player", function() {
+  // `this` is reset to an empty object before each spec, so we can stash things
+  // there using beforeEach (exercise: why can't we just do `var player = ...`?)
   beforeEach(function() {
-    player = new Player();
-    song = new Song("New Shoes");
+    this.player = new Player();
+    this.song = new Song("New Shoes");
   });
 
   describe("#play", function(){
     it("plays a song", function() {
-      player.play(song);
+      this.player.play(this.song);
 
-      expect(player.isPlaying).toBe(true);
-      expect(player.currentSong).toEqual(song);
+      expect(this.player.isPlaying).toBe(true);
+      expect(this.player.currentSong).toEqual(this.song);
     });
   });
 
   describe("#pause", function(){
     it("pauses the current song", function() {
-      player.play(song);
-      player.pause();
+      this.player.play(this.song);
+      this.player.pause();
 
-      expect(player.isPlaying).toBe(false);
+      expect(this.player.isPlaying).toBe(false);
     });
   });
 
   describe("#resume", function() {
     beforeEach(function() {
-      player.play(song);
-      player.pause();
+      this.player.play(this.song);
+      this.player.pause();
     });
 
     it("resumes after pausing", function() {
-      player.resume();
+      this.player.resume();
 
-      expect(player.isPlaying).toBe(true);
+      expect(this.player.isPlaying).toBe(true);
     });
 
     // Demonstrates expecting that a function call will throw an error
     it("throws an error if not paused", function() {
-      player.resume();
+      this.player.resume();
 
       expect(function() {
-        player.resume();
+        this.player.resume();
       }).toThrowError("song is already playing");
     });
   });
@@ -65,12 +51,12 @@ describe("Player", function() {
   // (note your Song itself does *not* need any functions on it for this to pass!)
   describe("#makeFavorite", function(){
     it("tells the current song to persist its favorite status", function() {
-      spyOn(song, 'persistFavoriteStatus');
+      spyOn(this.song, 'persistFavoriteStatus');
 
-      player.play(song);
-      player.makeFavorite();
+      this.player.play(this.song);
+      this.player.makeFavorite();
 
-      expect(song.persistFavoriteStatus).toHaveBeenCalledWith(true);
+      expect(this.song.persistFavoriteStatus).toHaveBeenCalledWith(true);
     });
   });
 });
