b18deada1d2e35de9745838a1443a00a2c99f0a6
George <gbanis@gmail.com>
2014-10-27 20:32:03 -0400

    Passes tests: play, pause, resume

--- src/Player.js
+++ src/Player.js
@@ -1,1 +1,17 @@
 // Player prototype needs to be defined here!
+function Player(){
+
+}
+
+Player.prototype = {
+  play: function(song){
+    this.isPlaying = true;
+    this.currentSong = song;
+  },
+  pause: function(){
+    this.isPlaying = false;
+  },
+  resume: function(){
+    this.isPlaying = true;
+  }
+};
--- src/Song.js
+++ src/Song.js
@@ -1,1 +1,11 @@
 // Song prototype needs to be defined here!
+function Song(){
+
+}
+
+Song.prototype = {
+  play: function(song){
+    this.isPlaying = true;
+    this.currentSong = song;
+  }
+};
